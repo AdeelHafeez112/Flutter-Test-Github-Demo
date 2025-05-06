8db3a49ac32c647971a51873cad1c20a96073e04
AdeelHafeez112 <adeelhafeez2019@gmail.com>
2025-05-06 13:34:14 +0500

    app bar widget added

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title:  const Text("Hello"),
       ),
       body: Center(
         child: Column(
